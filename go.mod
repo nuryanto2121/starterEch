eee2d08e0d13d565955d6eb5275625a96f20565b
nuryanto <nuryantofattih@gmail.com>
2020-02-19 16:14:04 +0700

    add

---
+++ go.mod
@@ -0,0 +1,23 @@
+module property/framwork
+
+go 1.12
+
+require (
+	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/jinzhu/now v1.1.1 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6 // indirect
+	golang.org/x/mod v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
+	golang.org/x/tools v0.0.0-20200216192241-b320d3a0f5a2 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+)
---
+++ models/sa/sa_user.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type SaUser struct {
+	UserID      int16     `json:"user_id" gorm:"PRIMARY_KEY"`
+	Passwd      string    `json:"passwd" gorm:"type:varchar(60);not null"`
+	GroupID     int16     `json:"group_id" gorm:"type:integer;not null"`
+	LevelNo     int16     `json:"level_no" gorm:"type:integer;default:0;not null"`
+	UserName    string    `json:"user_name" gorm:"type:varchar(60);not null"`
+	EmailAddr   string    `json:"email_addr" gorm:"type:varchar(100);unique_index"`
+	HandphoneNo string    `json:"handphone_no" gorm:"type:varchar(20)"`
+	CompanyID   int16     `json:"company_id" gorm:"type:integer;not null"`
+	ProjectID   int16     `json:"project_id" gorm:"type:integer;not null"`
+	PictureUrl  string    `json:"picture_url" gorm:"type:varchar(100)"`
+	UserStatus  int16     `json:"user_status" gorm:"type:integer;default:1"`
+	CreatedBy   string    `json:"created_by" gorm:"type:varchar(20);not null"`
+	CreatedAt   time.Time `json:"crated_at" gorm:"type:timestamp(0) without time zone;default:now()"`
+	UpdateBy    string    `json:"updated_by" gorm:"type:varchar(20);not null"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"type:timestamp(0) without time zone;default:now()"`
+}
